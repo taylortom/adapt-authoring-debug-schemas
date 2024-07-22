cddafb970857adad6bed2f4d454d9854ae73c6c1
Tom Taylor <hello@tomtaylor.name>
2024-07-22 18:08:02 +0100

    Fix plugin init

--- ui-plugins/debug-schemas/index.js
+++ ui-plugins/debug-schemas/index.js
@@ -3,10 +3,12 @@ define(function(require) {
   const Origin = require('core/origin');
   const SchemasView = require('./views/schemasView');
 
-  Origin.trigger(`debug:addView`, { 
-    name: 'schemas', 
-    icon: 'file', 
-    title: Origin.l10n.t('app.schemas'), 
-    view: SchemasView
+  Origin.on('debug:ready', () => {
+    Origin.trigger(`debug:addView`, { 
+      name: 'schemas', 
+      icon: 'file', 
+      title: Origin.l10n.t('app.schemas'), 
+      view: SchemasView
+    })
   })
 });
